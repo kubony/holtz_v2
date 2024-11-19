4cd86365208978b039fa02699ec3d8692070ec70
Inkeun Seo <kubony@gmail.com>
2024-11-19 14:05:26 +0900

    fix: import 경로 수정

--- pages/1_☕🧋🥤_서울창업허브_1층_더치앤빈.py
+++ pages/1_☕🧋🥤_서울창업허브_1층_더치앤빈.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import utils.chatbotutils as chatbotutils
 import streamlit as st
 from streaming import StreamHandler
